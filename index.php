5ce9376d78ad54280dfa5969e55cb387a83ca191
goods-sales <58511712+goods-sales@users.noreply.github.com>
2019-12-04 13:57:14 +0300

    Fist Commit
    Comit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>ttt</title>
+<meta charset="windows-1251">
+<meta http-equiv="content-language" content="ru">
+<meta name="keywords" content="ключевые слова">
+<meta name="description" content="описание страницы">
+</head>
+<body> Hello
+</body>
+</html>
